a1ca974bdf369453607ed9a12dd0f2ce1a532231
ivanj9 <ivanj9@gmail.com>
2020-04-26 00:08:14 +0200

    Add project files.

---
+++ shellLogin01/shellLogin01/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using shellLogin01.Models;
+
+namespace shellLogin01.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
